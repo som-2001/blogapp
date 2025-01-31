f9e9bcff1b3e02cfa164b11f6f38566e2a957ca2
som-2001 <100964164+som-2001@users.noreply.github.com>
2025-01-31 20:05:18 +0530

    Update Dateformater.js

--- src/components/Dateformater.js
+++ src/components/Dateformater.js
@@ -2,7 +2,7 @@ export const Dateformater=(date)=>{
    
     const year=date.split("-")[0];
     const month=date.split("-")[1];
-    const day=date.split("-")[2];
+    const day=date.split("-")[2]; 
 
     const monthName=["Jan","Feb","Mar","Apr","May","Jun","July","Aug","Sept","Oct","Nov","Dec"];
 
